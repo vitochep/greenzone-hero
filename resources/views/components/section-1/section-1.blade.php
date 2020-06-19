d46ff35f75aba6b19c0a262ee66ef467544765c6
Vitaliy <animus2393@gmail.com>
2020-06-19 18:02:52 +0300

    fix header components

--- resources/views/components/section-1/section-1.blade.php
+++ resources/views/components/section-1/section-1.blade.php
@@ -6,13 +6,42 @@
         <div class="section-wrapper">
             <div class="section-container">
                 <div class="header-elems">
-                    <img class="logo-browse" src="/image/Group4.png" alt="logo-star">
-                    <div class="section-buttons">
-                        <a class="join-button" href="#">
-                            <span>JOIN</span>
+                    <div class="header-elems_top">
+                        <img class="logo-browse" src="/image/Group4.png" alt="logo-star">
+                        <div class="section-buttons">
+                            <a class="join-button" href="#">
+                                <span>JOIN</span>
+                            </a>
+                            <a class="sign-in_button" href="#">
+                                <span>SIGN IN</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="social-links">
+                        <a href="#">
+                            <svg class="facebook">
+                                <use xlink:href="#facebook"></use>
+                            </svg>
                         </a>
-                        <a class="sign-in_button" href="#">
-                            <span>SIGN IN</span>
+                        <a href="#">
+                            <svg class="twitter">
+                                <use xlink:href="#twitter"></use>
+                            </svg>
+                        </a>
+                        <a href="#">
+                            <svg class="linkedin">
+                                <use xlink:href="#linkedin"></use>
+                            </svg>
+                        </a>
+                        <a href="#">
+                            <svg class="instagram">
+                                <use xlink:href="#instagram"></use>
+                            </svg>
+                        </a>
+                        <a href="#">
+                            <svg class="google-plus">
+                                <use xlink:href="#google-plus"></use>
+                            </svg>
                         </a>
                     </div>
                 </div>
