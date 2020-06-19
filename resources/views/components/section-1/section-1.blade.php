b7d17530d4b5318907d79a545bc58ac0c2fe3ef7
Vitaliy <animus2393@gmail.com>
2020-06-19 17:03:00 +0300

    fix header components

--- resources/views/components/section-1/section-1.blade.php
+++ resources/views/components/section-1/section-1.blade.php
@@ -2,8 +2,20 @@
     <div class="background-map">
         <img class="logo-star" src="/image/svg/logo-star.svg" alt="logo-star">
 
+
         <div class="section-wrapper">
             <div class="section-container">
+                <div class="header-elems">
+                    <img class="logo-browse" src="/image/Group4.png" alt="logo-star">
+                    <div class="section-buttons">
+                        <a class="join-button" href="#">
+                            <span>JOIN</span>
+                        </a>
+                        <a class="sign-in_button" href="#">
+                            <span>SIGN IN</span>
+                        </a>
+                    </div>
+                </div>
 
             </div>
         </div>
