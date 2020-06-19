f03a29f4e5b5dc73490c5401bf42d1765ce809df
Vitaliy <animus2393@gmail.com>
2020-06-19 16:03:23 +0300

    add section-1

--- resources/views/components/section-1/section-1.blade.php
+++ resources/views/components/section-1/section-1.blade.php
@@ -1,5 +1,11 @@
 <section class="section-1">
     <div class="background-map">
+        <img class="logo-star" src="/image/svg/logo-star.svg" alt="logo-star">
 
+        <div class="section-wrapper">
+            <div class="section-container">
+
+            </div>
+        </div>
     </div>
 </section>
