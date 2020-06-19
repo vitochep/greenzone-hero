954870ae8bc463a6c76f10adfe1e46a2aa9dcf8e
Vitaliy <animus2393@gmail.com>
2020-06-19 21:46:17 +0300

    fix section-2

--- resources/views/components/section-2/section-2.blade.php
+++ resources/views/components/section-2/section-2.blade.php
@@ -1,8 +1,9 @@
 <section class="section-2">
-    <img  class="group-star" src="/image/svg/group-star.svg" alt="group-star">
+    <img class="group-star" src="/image/svg/group-star.svg" alt="group-star">
 
     <div class="background_section-2">
-        <img  class="star-banner" src="/image/star-banner.png" alt="star-banner">
+        <img class="star-banner" src="/image/star-banner.png" alt="star-banner">
+        <img  class="logo-big" src="/image/svg/logo-big.svg" alt="logo-big">
 
         <div class="section-wrapper">
             <div class="section-container mod-section_2">
@@ -12,7 +13,39 @@
                 </div>
 
                 <div class="section-description">
-                    <img  class="description-image" src="/image/svg/description-image.svg" alt="/description-image">
+                    <img class="description-image" src="/image/svg/description-image.svg" alt="/description-image">
+
+                    <div class="description-table">
+                        <h3 class="description-table_title">
+                            Bert's Black Widow Harley-Davidson
+                        </h3>
+
+                        <div class="description-table_content">
+                            <div class="description-table_row">
+                                <span class="table-category_name">Business category:</span>
+                                <p class="table-category_value">Auto/Motorcycles Sales</p>
+                            </div>
+                            <div class="description-table_row">
+                                <span class="table-category_name">Address:</span>
+                                <p class="table-category_value">2224&nbsp;El Jobean Road
+                                    Port Charlotte, FL&nbsp;33948</p>
+                            </div>
+                            <div class="description-table_row">
+                                <span class="table-category_name">Business description:</span>
+                                <p class="table-category_value">We&nbsp;are here to&nbsp;bring the sweetest rides on&nbsp;two
+                                    wheels, Harley-Davidson<sup class="reg">&reg;</sup> motorcycles, to&nbsp;Port
+                                    Charlotte, in&nbsp;Southwest Florida.</p>
+                            </div>
+                            <div class="description-table_row">
+                                <span class="table-category_name">Contact:</span>
+                                <p class="table-category_value">(941) 883-8000</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="section-block">
+
+                    </div>
                 </div>
             </div>
         </div>
